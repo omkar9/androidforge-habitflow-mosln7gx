e71a2974116dc990c71a60f29ae66a10f065f894
omkar9 <omkar946@gmail.com>
2026-05-05 17:59:53 +0530

    fix: update HabitFlowApp.kt

--- app/src/main/java/com/androidforge/habitflow/HabitFlowApp.kt
+++ app/src/main/java/com/androidforge/habitflow/HabitFlowApp.kt
@@ -2,15 +2,6 @@ package com.androidforge.habitflow
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
 
 @HiltAndroidApp
-class HabitFlowApp : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
-    }
-}
+class HabitFlowApp : Application()
