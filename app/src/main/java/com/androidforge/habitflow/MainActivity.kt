b90e7669c79e39f3e7b99796fa7b5b52006aa640
omkar9 <omkar946@gmail.com>
2026-05-05 17:59:50 +0530

    fix: update MainActivity.kt

--- app/src/main/java/com/androidforge/habitflow/MainActivity.kt
+++ app/src/main/java/com/androidforge/habitflow/MainActivity.kt
@@ -3,40 +3,19 @@ package com.androidforge.habitflow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
-import com.androidforge.habitflow.presentation.navigation.NavGraph
-import com.androidforge.habitflow.presentation.theme.HabitFlowTheme
-import com.androidforge.habitflow.presentation.util.AdmobManager
-import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    @Inject
-    lateinit var admobManager: AdmobManager
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Initialize AdMob SDK
-        MobileAds.initialize(this) {}
-
         setContent {
-            HabitFlowTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val navController = rememberNavController()
-                    NavGraph(navController = navController, admobManager = admobManager)
-                }
+            MaterialTheme {
+                Surface { Text("HabitFlow") }
             }
         }
     }
